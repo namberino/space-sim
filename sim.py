3567f076fb9797c189fdafa01f0193b839e02047
namber0 <namnb2402@gmail.com>
2024-05-28 21:32:30 +0700

    Add spacecraft object

--- sim.py
+++ sim.py
@@ -10,9 +10,9 @@ BLUE = (0, 0, 255)
 
 # simulation constants
 PLANET_MASS = 100
-OBJECT_MASS = 5
+SPACECRAFT_MASS = 5
 PLANET_SIZE = 50 # radius
-OBJECT_SIZE = 5 # radius
+SPACECRAFT_SIZE = 5 # radius
 VELOCITY_SCALE = 100
 G = 5
 
@@ -24,12 +24,23 @@ pygame.display.set_caption("Slingshot Simulation")
 background = pygame.transform.scale(pygame.image.load("img/background.jpg"), (WIN_WIDTH, WIN_HEIGHT))
 planet_img = pygame.transform.scale(pygame.image.load("img/jupiter.png"), (PLANET_SIZE * 2, PLANET_SIZE * 2))
 
+class Spacecraft:
+    def __init__(self, x, y, xvel, yvel, mass):
+        self.x = x
+        self.y = y
+        self.xvel = xvel
+        self.yvel = yvel
+        self.mass = mass
+
+    def draw(self):
+        pygame.draw.circle(window, RED, (int(self.x), int(self.y)), SPACECRAFT_SIZE)
+
 def main():
     clk = pygame.time.Clock()
     run = True
 
-    objs = []
-    tmp_obj_pos = None # stores position of object that has not been launched
+    spacecrafts = []
+    tmp_spacecraft_pos = None # stores position of object that has not been launched
 
     while run:
         mouse_pos = pygame.mouse.get_pos()
@@ -40,12 +51,22 @@ def main():
                 break
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                tmp_obj_pos = mouse_pos
+                if tmp_spacecraft_pos: # a spacecraft has already been placed
+                    tmp_x, tmp_y = tmp_spacecraft_pos
+                    craft = Spacecraft(tmp_x, tmp_y, 0, 0, SPACECRAFT_MASS)
+                    spacecrafts.append(craft)
+                    tmp_spacecraft_pos = None
+                else:
+                    tmp_spacecraft_pos = mouse_pos
 
         window.blit(background, (0, 0))
 
-        if tmp_obj_pos:
-            pygame.draw.circle(window, RED, tmp_obj_pos, OBJECT_SIZE)
+        if tmp_spacecraft_pos:
+            pygame.draw.line(window, WHITE, tmp_spacecraft_pos, mouse_pos, 2) # draw line from object to mouse
+            pygame.draw.circle(window, RED, tmp_spacecraft_pos, SPACECRAFT_SIZE)
+
+        for craft in spacecrafts:
+            craft.draw()
 
         pygame.display.update()
 
