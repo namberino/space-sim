63f7e7f1033408e9ecd29ca895bb1d185d710923
namber0 <namnb2402@gmail.com>
2024-05-28 21:20:16 +0700

    Add object spawning

--- sim.py
+++ sim.py
@@ -10,7 +10,7 @@ BLUE = (0, 0, 255)
 
 # simulation constants
 PLANET_MASS = 100
-SPACECRAFT_MASS = 5
+OBJECT_MASS = 5
 PLANET_SIZE = 50 # radius
 OBJECT_SIZE = 5 # radius
 VELOCITY_SCALE = 100
@@ -21,17 +21,35 @@ pygame.init()
 window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption("Slingshot Simulation")
 
-background_img = pygame.transform.scale(pygame.image.load("img/background.jpg"), (WIN_WIDTH, WIN_HEIGHT))
+background = pygame.transform.scale(pygame.image.load("img/background.jpg"), (WIN_WIDTH, WIN_HEIGHT))
 planet_img = pygame.transform.scale(pygame.image.load("img/jupiter.png"), (PLANET_SIZE * 2, PLANET_SIZE * 2))
 
 def main():
+    clk = pygame.time.Clock()
     run = True
 
+    objs = []
+    tmp_obj_pos = None # stores position of object that has not been launched
+
     while run:
+        mouse_pos = pygame.mouse.get_pos()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                tmp_obj_pos = mouse_pos
+
+        window.blit(background, (0, 0))
+
+        if tmp_obj_pos:
+            pygame.draw.circle(window, RED, tmp_obj_pos, OBJECT_SIZE)
+
+        pygame.display.update()
+
+        clk.tick(FPS)
 
     pygame.quit()
 
